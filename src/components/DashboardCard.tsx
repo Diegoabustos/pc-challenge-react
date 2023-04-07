89e905d9d139c0fe734e1de7eaf2dc24b854375f
Diegoabustos <balorcode22@gmail.com>
2023-04-07 11:01:33 -0600

    style: 💄 Modify icon percentage revenue in dash card

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,35 +1,33 @@
-
 type DashboardCardProps = {
-  description: string
+  description: string;
   value: [] | string;
-}
+  color?: string
+};
 
-export const DashboardCard = ({value, description}: DashboardCardProps) => {
-
-return (
-  <div className="flex items-center p-8 bg-white shadow rounded-lg h-2/5">
-    <div className="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-green-600 bg-green-100 rounded-full mr-6">
-      <svg
-        aria-hidden="true"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        className="h-6 w-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-        />
-      </svg>
-    </div>
-    <div>
-      <span className="block text-gray-500">{description}</span>
-      <span className="block text-4xl font-bold">
-        {value}
+export const DashboardCard = ({ value, description, color }: DashboardCardProps) => {
+  return (
+    <div className="flex items-center justify-between p-8 bg-white shadow rounded-lg h-2/5">
+      <div>
+        <span className="block text-gray-500">{description}</span>
+        <span className="block text-4xl font-bold">{value}</span>
+      </div>
+      <span className="flex items-center px-5 py-2 mx-2 mb-20 text-sm rounded-full text-red-600 bg-red-100 dark:bg-red-900 dark:text-red-300">
+        <svg
+          className="w-4 h-4"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6 9L12 15L18 9"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+        <span>10.8%</span>
       </span>
     </div>
-  </div>
-);
+  );
 };
